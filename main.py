ad809961b6f1bf6e74214c6a62bfb42cf9f5b4d0
Muhammad <mtg111@hotmail.com>
2024-06-09 23:33:06 +0100

    updated feature finding funcs

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 import pandas as pd
 import numpy as np
 import os
+from pathlib import Path
+from matplotlib import pyplot as plt
+from scipy.signal import savgol_filter
+from scipy.signal import find_peaks
+
 #always excute in cwd - place data files in code folder
 from os.path import abspath, dirname
 os.chdir(dirname(abspath(__file__)))
-
-from pathlib import Path
-from matplotlib import pyplot as plt
 
 def GetDataAndConcat(path):
     #list and sort files
@@ -46,16 +48,28 @@ def RemoveNanAndZeros(df):
     return df, droppedCollumns
 
 
-def NormaliseData(df):
-    means = df.mean()
+def NormaliseDataAndSmooth(df):
+    smooteddf = pd.DataFrame()
     for col in df:
-        df[col] = df[col] - df[col].mean()
-    return df
+        smooteddf[col] = df[col] - df[col].mean() #remove baseline
+        smooteddf[col] = smooteddf[col]/abs(smooteddf[col]).max()
+        smooteddf[col] = savgol_filter(smooteddf[col] ,41, 3) # window size 51, polynomial order 3
+    return smooteddf
+
+# def findandPlotPeaks(filteredData, thresholdValue):
+#     plt.figure()
+#     for col in filteredData:
+#         peaks = find_peaks(filteredData[col], height=(-thresholdValue, thresholdValue))
+#         # plt.plot(-thresholdValue, "--", color="gray")
+#         # plt.plot(thresholdValue, ":", color="gray")
+#         plt.plot(filteredData[col])
+#         for peak in peaks:
+#             plt.plot(peaks[peak] , filteredData[col][peaks], 'X')
 
 
-def plotAll(df):
+def plotAll(df,numtoPlot):
     plt.figure()
-    for col in df:
+    for i , col in enumerate(df):
         x = np.arange(0 ,len(df[col]))
         y = df[col]
         #plt.xticks(x, my_xticks ,rotation = 45, fontsize =10)
@@ -63,12 +77,21 @@ def plotAll(df):
         plt.grid(True)
         plt.xlabel('sample No')
         plt.ylabel('Value a.u')
+        if (i == numtoPlot):
+            break
+
         #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         #plt.savefig('rollingMonth.png', dpi = 300 ,  bbox_inches='tight')
 
 
+
+#%%
 #path to data files
 path = 'datasets/'
+
+
+###############################################################
+#DATA PREPERATION
 #read in datasets and concat as per file order
 data = GetDataAndConcat(path)
 
@@ -76,13 +99,32 @@ data = GetDataAndConcat(path)
 #datasts that contains nan are kept in droppedData dataframe
 data, droppedData = RemoveNanAndZeros(data)
 
-#baseline dataset
-NormaliseData(data) 
+#baseline datasets using mean values
+filteredData = NormaliseDataAndSmooth(data) 
 
-plotAll(data)
-
-
-
+#plot 30 datasets to investiagate feetures
+plotAll(filteredData,30)
 
 
+###############################################################
+#FEATURE EXTRACTION
+#plot the max and min differtiated dataset to pick the transition points
+def find_Transitions(filteredData,filestoplot):
+    for i, col in enumerate(filteredData):
+        plt.figure()
+        max = filteredData[col].diff().idxmax()
+        min = filteredData[col].diff().idxmin()
+        plt.plot(filteredData[col])
+        plt.axvline(max)
+        plt.axvline(min)
+        if (i == filestoplot):
+            break
+        
+
+
+
+
+
+# %%
+find_Transitions(filteredData,0)
 # %%
